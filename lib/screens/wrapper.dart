ba6b4a898da9564e09c38d7414d2f5c8285fa245
LudicrousWhale <mshivam2012@gmail.com>
2020-06-17 14:57:38 +0530

    next

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,15 @@
+import 'package:coffee_order/models/user.dart';
 import 'package:coffee_order/screens/authenticate/authenticate.dart';
 import 'package:coffee_order/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final user = Provider.of<User>(context);
+    print(user);
     
     //return either Home or Authenticate widget
     return Authenticate();
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -6,8 +6,14 @@ class AuthService {
 
   //create user obj based on FirebaseUser 
 
-  User _userFromFirebase(FirebaseUser user) {
+  User _userFromFirebaseUser(FirebaseUser user) {
     return user != null ? User(uid: user.uid) : null;
+  }
+
+  //auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+      .map(_userFromFirebaseUser);
   }
 
   //sign in anon
@@ -15,7 +21,7 @@ class AuthService {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return _userFromFirebase(user);
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
